10b77791a16fc0974f77a193469907278abe6115
Ketan K <keshava.ke@northeastern.edu>
2024-03-11 19:20:06 -0400

    Updating

--- speakers.php
+++ speakers.php
@@ -185,6 +185,24 @@
                             <p class="card-text"> DS &AD, SAC, ISRO</p>
                         </div>
                     </div>
+                    <!-- <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/dinesh.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Shri D K Singh</h4>
+                            <p class="card-text"> DS &AD, SAC, ISRO</p>
+                        </div>
+                    </div>
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                        <center>
+                            <img class="card-img-top" src="./images/speakers/dinesh.jpg" alt="Card image">
+                        </center>
+                        <div class="card-body">
+                            <h4 class="card-title">Shri D K Singh</h4>
+                            <p class="card-text"> DS &AD, SAC, ISRO</p>
+                        </div>
+                    </div> -->
                 </div>
             </div>
         </div>
