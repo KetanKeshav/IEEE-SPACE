f0e0348aa446a3d11dba59658901692f81c107f5
Ketan K <keshava.ke@northeastern.edu>
2024-03-22 18:27:48 -0400

    Updating

--- speakers.php
+++ speakers.php
@@ -187,7 +187,7 @@
 
                     <div class="card col-lg-4 col-md-4 col-sm-6">
                     <center>
-                    <img class="card-img-top" src="./images/Unnikrishnan.jpg" alt="Card image">
+                    <img class="card-img-top" src="./images/speakers/Unnikrishnan.jpg" alt="Card image">
                     </center>
                     <div class="card-body">
                     <h4 class="card-title">Dr S. Unnikrishnan Nair</h4>
