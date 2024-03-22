b1fadc6db6398053afb7c8181d06060d101d6ac7
Ketan K <keshava.ke@northeastern.edu>
2024-03-22 18:27:42 -0400

    Updating

--- speakers.php
+++ speakers.php
@@ -184,7 +184,15 @@
                             <p class="card-text"> DS & AD, SAC, ISRO</p>
                         </div>
                     </div>
-                    
+
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
+                    <center>
+                    <img class="card-img-top" src="./images/Unnikrishnan.jpg" alt="Card image">
+                    </center>
+                    <div class="card-body">
+                    <h4 class="card-title">Dr S. Unnikrishnan Nair</h4>
+                    <p class="card-text">DS and Director, VSSC & IIST, ISRO</p>
+                    </div>
             </div>
         </div>
     </section>
