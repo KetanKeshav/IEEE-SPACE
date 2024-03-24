e6cded27f24d8423c6db705c4d84d031a8bfc41b
Ketan K <keshava.ke@northeastern.edu>
2024-03-24 00:19:49 -0400

    Updating

--- speakers.php
+++ speakers.php
@@ -165,7 +165,7 @@
                             <p class="card-text">VC, Gati Shakti Vishwavidyalaya</p>
                         </div>
                     </div>
-                    <!-- <div class="card col-lg-4 col-md-4 col-sm-6">
+                    <div class="card col-lg-4 col-md-4 col-sm-6">
                         <center>
                             <img class="card-img-top" src="./images/speakers/tapan.png" alt="Card image">
                         </center>
@@ -173,7 +173,7 @@
                             <h4 class="card-title">Dr. Tapan Misra</h4>
                             <p class="card-text">DS & Former Director, SAC</p>
                         </div>
-                    </div> -->
+                    </div>
                 </div>
 
                 <!-- <div class="row"> -->
