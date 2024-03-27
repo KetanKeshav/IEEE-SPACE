a06e1cbe723e762f56c4ed834a74624ce5912541
Ketan K <keshava.ke@northeastern.edu>
2024-03-27 03:09:43 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -1,16 +1,16 @@
   <!-- sponsors section -->
 
   <section class="about_section layout_padding">
-      <!-- <div class="container  "> -->
+      <div class="container  ">
           <div class="heading_container">
               <h2 style="text-align: center;"><span>Organised by</span></h2>
           </div>
-          <div class="row">
+          <!-- <div class="row"> -->
               <div class="col-md-12 img-box mt-2 mb-2">
                 <a href=""><img src="./images/sponsors/oc2.png" alt="" style="width: 700px; height: 300px;"><br><br></a>
               </div>
-          </div>
-      <!-- </div> -->
+          <!-- </div> -->
+      </div>
   </section>
 
   <!-- end sponsprs section -->
