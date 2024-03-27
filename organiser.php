2cfb8d8d169057bef42b019f1da0db8365a3aa64
Ketan K <keshava.ke@northeastern.edu>
2024-03-27 03:01:08 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -6,17 +6,16 @@
               <h2 style="text-align: center;"><span>Organised by</span></h2>
           </div>
           <div class="row">
-              <div class="col-md-6 img-box mt-2 mb-2">
-                  <a href=""><img src="./images/sponsors/aess.png" alt=""><br><br>
+              <div class="col-md-12 img-box mt-2 mb-2">
+                  <a href=""><img src="./images/sponsors/oc2.png" alt=""><br><br>
                       <!-- <h2 style="text-align: center;">Diamond</h2> -->
                   </a>
               </div>
-              <div class="col-md-6 img-box mt-2 mb-2">
+              <!-- <div class="col-md-6 img-box mt-2 mb-2">
                   <a href=""><img src="./images/sponsors/apsmtts.png" alt=""><br><br>
-                  
-                      <!-- <h2 style="text-align: center;">Diamond</h2> -->
+
                   </a>
-              </div>
+              </div> -->
               <!-- <div class="col-md-4 img-box mt-2 mb-2">
                   <a href="https://www.mvg-world.com/en"><img src="https://ieeespace.org/images/sponsors/diamond.png" alt=""><br><br>
                       <h2 style="text-align: center;">Diamond</h2>
