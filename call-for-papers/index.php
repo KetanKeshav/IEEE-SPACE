4623e6b5bb0bf414eb2c8afa42e5f55298401d03
Ketan K <keshava.ke@northeastern.edu>
2024-03-28 01:49:11 -0400

    Updating

--- call-for-papers/index.php
+++ call-for-papers/index.php
@@ -185,7 +185,7 @@
 
   <center>
     <div class="detail_box">
-        <a href="downloads/cfp.pdf" class="button-con" download>
+        <a href="./downloads/cfp.pdf" class="button-con" download>
             <h1>Call for Paper</h1>
         </a>
     </div>
