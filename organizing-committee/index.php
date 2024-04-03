b647bf1571f3a0f433beaf6f52a503cdbec0a52f
Ketan K <keshava.ke@northeastern.edu>
2024-04-02 22:09:31 -0400

    updating

--- organizing-committee/index.php
+++ organizing-committee/index.php
@@ -349,12 +349,14 @@
 
         <div class="container">
   <div class="row">
+    <center>
     <div class="col-md-6">
       <h2><span class="span_head">Finance Chair:</span></h2>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-6">
       <h2><span class="span_head">Europian Chair:</span></h2>
     </div>
+  </center>
   </div>
 
   <div class="row">
