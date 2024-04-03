d32a37e6d1a30a2f5ccb8f7c8b3d6cd9c8080c63
Ketan K <keshava.ke@northeastern.edu>
2024-04-02 23:23:08 -0400

    updating

--- organizing-committee/index.php
+++ organizing-committee/index.php
@@ -361,10 +361,10 @@
     <!-- First Person -->
     <div class="card col-lg-6 col-md-6 col-sm-6">
   <div class="row">
-    <div class="col-12 text-left"> <!-- Adjusted column class to align content to the left -->
+    <div class="col-12"> <!-- Adjusted column class to align content to the left -->
       <img class="card-img-top" src="./images/Mahesh.webp" alt="Card image">
     </div>
-    <div class="col-12 text-left">
+    <div class="col-12">
     <div class="card-body"> <!-- Added text-left class to align content to the left -->
       <h4 class="card-title">Mahesh A</h4>
       <p class="card-text">Treasurer, IEEE AESS Bangalore Chapter</p>
