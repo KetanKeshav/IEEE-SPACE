f8df91e4947b120e89ab67d72ec23702cb381925
Ketan K <keshava.ke@northeastern.edu>
2024-04-11 15:39:22 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -7,14 +7,14 @@
         </div>
         <div class="organisers_row" style="display: flex; justify-content: center;">
             <div class="organiser_logo">
-                <img src="./images/sponsors/1.png"" alt="Organiser 1" style="width: 100px; height: auto;">
+                <img src="./images/sponsors/1.png"" alt="Organiser 1" style="width: 200px; height: auto;">
             </div>
             <div class="organiser_logo">
-                <img src="./images/sponsors/1.png" alt="Organiser 2" style="width: 100px; height: auto;">
+                <img src="./images/sponsors/1.png" alt="Organiser 2" style="width: 200px; height: auto;">
             </div>
-            <div class="organiser_logo">
+            <!-- <div class="organiser_logo">
                 <img src="organiser3_logo.png" alt="Organiser 3" style="width: 100px; height: auto;">
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
