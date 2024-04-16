fb1aea3a768109ffbc6ae48216cc6c409cc7ab86
Ketan K <keshava.ke@northeastern.edu>
2024-04-16 00:55:15 -0400

    Testing v1 to v2

--- organiser.php
+++ organiser.php
@@ -6,10 +6,7 @@
         </div>
         <div class="organisers_row">
             <div class="organiser_logo">
-                <img src="./images/sponsors/2.png" alt="Organiser 1">
-            </div>
-            <div class="organiser_logo">
-                <img src="./images/sponsors/1.png" alt="Organiser 2">
+                <img src="./images/sponsors/1.png" alt="Organiser 1">
             </div>
         </div>
     </div>
@@ -20,16 +17,7 @@
         </div>
         <div class="organisers_row">
             <div class="organiser_logo">
-                <img src="./images/sponsors/5.png" alt="Organiser 1">
-            </div>
-            <div class="organiser_logo">
-                <img src="./images/sponsors/4.png" alt="Organiser 2">
-            </div>
-            <div class="organiser_logo">
-                <img src="./images/sponsors/7.png" alt="Organiser 3">
-            </div>
-            <div class="organiser_logo">
-                <img src="./images/sponsors/3.png" alt="Organiser 4">
+                <img src="./images/sponsors/2.png" alt="Organiser 2">
             </div>
         </div>
     </div>
