a826cee3359b96075d470a29d0b4851432b0f895
Ketan K <keshava.ke@northeastern.edu>
2024-04-17 09:19:45 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -1,15 +1,15 @@
 <!-- sponsors section -->
-<section class="about_section layout_padding">
-    <div class="container">
-        <div class="sponsors_row">
-            <div class="sponsors_column">
-                <div class="heading_container">
+<section class="about_section layout_padding" style="display: flex; justify-content: space-between;">
+    <div class="container" style="flex: 1;">
+        <div class="sponsors_row" style="display: flex; justify-content: space-between;">
+            <div class="sponsors_column" style="flex: 1; margin-right: 10px;">
+                <div class="heading_container" style="text-align: center;">
                     <h2 class="section_heading"><span>Organised by</span></h2>
                     <img src="./images/sponsors/1.png" alt="Organiser 1">
                 </div>
             </div>
-            <div class="sponsors_column">
-                <div class="heading_container">
+            <div class="sponsors_column" style="flex: 1; margin-left: 10px;">
+                <div class="heading_container" style="text-align: center;">
                     <h2 class="section_heading"><span>Technical Co-Sponsors</span></h2>
                     <img src="./images/sponsors/2.png" alt="Organiser 2">
                 </div>
@@ -17,27 +17,3 @@
         </div>
     </div>
 </section>
-
-.about_section {
-    display: flex;
-    justify-content: space-between;
-}
-
-.sponsors_row {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-}
-
-.sponsors_column {
-    flex: 1;
-    margin: 0 10px; /* Adjust margins as needed */
-}
-
-.heading_container {
-    text-align: center;
-}
-
-.section_heading span {
-    display: block;
-}
