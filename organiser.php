583c3fad7f3a4078e7aaac2ad08f409f2f017b3d
Ketan K <keshava.ke@northeastern.edu>
2024-04-17 09:18:27 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -1,16 +1,43 @@
 <!-- sponsors section -->
 <section class="about_section layout_padding">
     <div class="container">
-        <div class="heading_container">
-            <h2 class="section_heading"><span>Organised by</span></h2>
-            <img src="./images/sponsors/1.png" alt="Organiser 1">
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="heading_container">
-            <h2 class="section_heading"><span>Technical Co-Sponsors</span></h2>
-            <img src="./images/sponsors/2.png" alt="Organiser 2">
+        <div class="sponsors_row">
+            <div class="sponsors_column">
+                <div class="heading_container">
+                    <h2 class="section_heading"><span>Organised by</span></h2>
+                    <img src="./images/sponsors/1.png" alt="Organiser 1">
+                </div>
+            </div>
+            <div class="sponsors_column">
+                <div class="heading_container">
+                    <h2 class="section_heading"><span>Technical Co-Sponsors</span></h2>
+                    <img src="./images/sponsors/2.png" alt="Organiser 2">
+                </div>
+            </div>
         </div>
     </div>
 </section>
+
+.about_section {
+    display: flex;
+    justify-content: space-between;
+}
+
+.sponsors_row {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.sponsors_column {
+    flex: 1;
+    margin: 0 10px; /* Adjust margins as needed */
+}
+
+.heading_container {
+    text-align: center;
+}
+
+.section_heading span {
+    display: block;
+}
