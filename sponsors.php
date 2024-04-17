4c327302219b43e76b6dccfd9c120b3bc75c13ba
Ketan K <keshava.ke@northeastern.edu>
2024-04-17 08:50:34 -0400

    Updating

--- sponsors.php
+++ sponsors.php
@@ -6,8 +6,7 @@
               <h2 style="text-align: center;"><span>Sponsors</span></h2>
           </div>
           <br>
-          <h2>Coming soon</h2>
-          <<div class="row">
+          <div class="row">
               <div class="col-md-4 img-box mt-2 mb-2">
                   <a href="https://www.ansys.com/en-in"><img src="https://ieeespace.org/images/sponsors/1n.png" alt=""><br><br>
                       <h2 style="text-align: center;">Signature</h2>
