65351cd3a9b65ae7d5c771d8e6644d6eb883e9f2
Ketan K <keshava.ke@northeastern.edu>
2024-04-17 09:20:41 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -2,13 +2,13 @@
 <section class="about_section layout_padding" style="display: flex; justify-content: space-between;">
     <div class="container" style="flex: 1;">
         <div class="sponsors_row" style="display: flex; justify-content: space-between;">
-            <div class="sponsors_column" style="flex: 1; margin-right: 10px;">
+            <div class="sponsors_column" style="flex: 1; margin-right: 5px;">
                 <div class="heading_container" style="text-align: center;">
                     <h2 class="section_heading"><span>Organised by</span></h2>
                     <img src="./images/sponsors/1.png" alt="Organiser 1">
                 </div>
             </div>
-            <div class="sponsors_column" style="flex: 1; margin-left: 10px;">
+            <div class="sponsors_column" style="flex: 1; margin-left: 5px;">
                 <div class="heading_container" style="text-align: center;">
                     <h2 class="section_heading"><span>Technical Co-Sponsors</span></h2>
                     <img src="./images/sponsors/2.png" alt="Organiser 2">
