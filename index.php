0855343c73c57d42688f0512c1545d7df6c1d020
Ketan K <keshava.ke@northeastern.edu>
2024-04-18 14:21:20 -0400

    Updating

--- index.php
+++ index.php
@@ -69,6 +69,7 @@
                     <h3>(SPace, Aerospace and defenCE conference)</h3>
                     <h2><b>July 22-23, 2024</b></h2>
                     <h2><b>Sheraton Grand Bengaluru Whitefield Hotel & Convention Center</b></h2> -->
+                    <br><br><br><br>
                     <a href="./downloads/cfp.pdf" target="_blank" download="Program Booklet">
                       Call For Paper
                     </a>
@@ -90,6 +91,7 @@
                     <h3>(SPace, Aerospace and defenCE conference)</h3>
                     <h2><b>July 22-23, 2024</b></h2>
                     <h2><b>Sheraton Grand Bengaluru Whitefield Hotel & Convention Center</b></h2> -->
+                    <br><br><br><br>
                     <a href="./downloads/cfp.pdf" target="_blank" download="Program Booklet">
                       Call For Paper
                     </a>
@@ -114,6 +116,7 @@
                     <h3>(SPace, Aerospace and defenCE conference)</h3>
                     <h2><b>July 22-23, 2024</b></h2>
                     <h2><b>Sheraton Grand Bengaluru Whitefield Hotel & Convention Center</b></h2> -->
+                    <br><br><br><br>
                     <a href="./downloads/cfp.pdf" target="_blank" download="Program Booklet">
                       Call For Paper
                     </a>
