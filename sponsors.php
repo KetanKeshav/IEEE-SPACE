4106a069996e68383a91c752cee3b6266ed4c9a9
Ketan K <keshava.ke@northeastern.edu>
2024-04-20 14:20:03 -0400

    Updating

--- sponsors.php
+++ sponsors.php
@@ -11,14 +11,14 @@
               </div>
           </div>
           <div class="heading_container">
-              <h2 style="text-align: center;"><span>Diamond Sponsor</span></h2>
+              <h2 style="text-align: center;"><span>Diamond Sponsors</span></h2>
           </div>
           <div class="row">
               <div class="col-md-4 img-box mt-2 mb-2">
               <a href="https://www.anritsu.com/en-us/"><img src="https://ieeespace.org/images/sponsors/diamond-1.png" alt=""></a>
               </div>
               <div class="col-md-4 img-box mt-2 mb-2">
-              <a href="https://www.ni.com/en.html"><img src="https://ieeespace.org/images/sponsors/diamond-2.png" alt=""></a>
+              <a href="https://www.ni.com/en.html"><img src="https://ieeespace.org/images/sponsors/diamond-2-a.png" alt=""></a>
               </div>
           </div>
           <!-- <div class="heading_container">
