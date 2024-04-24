a86193d56c67d698cfe9eacd1ee6948fbfc976b9
Ketan K <keshava.ke@northeastern.edu>
2024-04-24 10:57:18 -0400

    Updating

--- organiser.php
+++ organiser.php
@@ -1,17 +1,11 @@
 <!-- sponsors section -->
-<section class="about_section layout_padding" style="display: flex; justify-content: space-between;">
-    <div class="container" style="flex: 1;">
-        <div class="sponsors_row" style="display: flex; justify-content: space-between;">
-            <div class="sponsors_column" style="flex: 1; margin-right: 1px;">
+<section class="about_section layout_padding" style="display: flex; justify-content: center;">
+    <div class="container">
+        <div class="sponsors_row" style="display: flex; justify-content: center;">
+            <div class="sponsors_column">
                 <div class="heading_container" style="text-align: center;">
-                    <h2 class="section_heading"><span>Organised by</span></h2>
-                    <img src="./images/sponsors/1.png" alt="Organiser 1">
-                </div>
-            </div>
-            <div class="sponsors_column" style="flex: 1; margin-left: 1px;">
-                <div class="heading_container" style="text-align: center;">
-                    <h2 class="section_heading"><span>Technical Co-Sponsors</span></h2>
-                    <img src="./images/sponsors/2.png" alt="Organiser 2">
+                    <!-- <h2 class="section_heading"><span>Organised by</span></h2> -->
+                    <img src="./images/sponsors/sponsor-1.jpg" alt="Organisers">
                 </div>
             </div>
         </div>
