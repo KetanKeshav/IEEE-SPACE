38b43576a5a2b8faee108eded645ebbdc7b92421
Ketan K <keshava.ke@northeastern.edu>
2024-05-23 10:47:48 -0400

    updating

--- sponsors.php
+++ sponsors.php
@@ -1,6 +1,6 @@
   <!-- sponsors section -->
 
-  <section class="about_section layout_padding">
+  <!-- <section class="about_section layout_padding">
       <div class="container">
           <div class="heading_container">
               <h2 style="text-align: center;"><span>Signature Sponsor</span></h2>
@@ -29,14 +29,6 @@
               <a href="https://www.keysight.com/us/en/home.html"><img src="https://ieeespace.org/images/sponsors/platinum.png" alt=""></a>
               </div>
           </div>
-          <!-- <div class="heading_container">
-              <h2 style="text-align: center;"><span>Gold Sponsor</span></h2>
-          </div>
-          <div class="row">
-              <div class="col-md-4 img-box mt-2 mb-2">
-              <h2>Coming soon</h2>
-              </div>
-          </div> -->
           <div class="heading_container">
               <h2 style="text-align: center;"><span>Silver Sponsor</span></h2>
           </div>
@@ -49,10 +41,21 @@
               </div>
           </div>
          
-  </section>
+  </section> -->
 
   <!-- end sponsprs section -->
 
+  <section class="about_section layout_padding" style="display: flex; justify-content: center;">
+    <div class="container" style="max-width: 100%; overflow: hidden;">
+        <div class="sponsors_row" style="display: flex; justify-content: center;">
+            <div class="sponsors_column">
+                <div class="heading_container" style="text-align: center;">
+                    <img src="./images/sponsors/sponsors-main.png" alt="Organisers" style="max-width: 100%;">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
   <!-- sponsors section -->
 <section class="about_section layout_padding" style="display: flex; justify-content: center;">
