cc48fc455823f23573fa5865e5f0611b36675849
Ketan K <ketan.keshav7@gmail.com>
2024-06-29 01:26:46 -0400

    updating

--- milli.php
+++ milli.php
@@ -13,7 +13,7 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <title>Millimeter-Wave MMICs for Front-End Applications in Aerospace and Defense | SPACE 2024</title>
+    <title>Workshop on “Create Physics based High-Fidelity Space Mission Using Simulation” | SPACE 2024</title>
 
     <link rel="shortcut icon" href="./images/favicon.ico" type="">
 
@@ -53,80 +53,31 @@
     <div class="container">
       <div class="heading_container heading_center">
         <h2 style="text-align: center;">
-            Workshop on Millimeter-Wave MMICs for Front-End Applications in Aerospace and Defense<br>
-            <a href="https://www.linkedin.com/in/asmita-dani-rf?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Dr. Asmita Dani</a>
+            Workshop on “Create Physics based High-Fidelity Space Mission Using Simulation”<br>
+            July 21, 2024 (11.00AM - 1.00PM)
         </h2><br>
       </div>
       <div class="cfa_heading">
         <p style="text-align: justify;">
-            <b>Introduction:</b><br>
-            Millimeter-wave MMICs (Monolithic Microwave Integrated Circuits) have emerged as critical
-            components in modern aerospace and defense systems, enabling advanced radar, communication,
-            and electronic warfare capabilities. This workshop aims to provide participants with a
-            comprehensive understanding of millimeter-wave MMIC technology and its applications in
-            front-end systems for space and defense industries.
+            Space is always filled with mystery, and everyone is excited to hear about space exploration.
+            Since there are many unknowns in space, the current achievements in space technologies
+            are gathered from knowledge collected over the years by different countries. Unlike other
+            areas, space organizations cannot afford failures in their missions, and first-time success is a
+            major milestone. This workshop session will help the audience learn about the different
+            stages of space missions and how simulation tools can help the space community model the
+            entire mission virtually before creating a prototype. The agenda for the sessions is as follows:
+        </p>
+        <ul>
+            <li>Create a space mission from the requirements (spacecraft launch, orbit determination, satellite orbital maneuvering).</li>
+            <li>Protect satellites from harsh space environments.</li>
+            <li>Check the performance of space communication, radar, and EOIR systems.</li>
+        </ul>
+        <p><b>Speakers:</b></p>
+        <p style="text-align: justify;">
+            <b>Nijas Kunju:</b> Nijas is a Principal Application Engineer in Application Engineering at ANSYS Inc. He specializes in the domain of RF, Antennas, Radar, Signal Integrity, Power Integrity, and Multiphysics simulation. Nijas has been working with ANSYS for the last 8 years. He provides technical assistance for RF components, Antennas, Phased arrays, Radar, Automotive, and 5G/6G applications. He has been interacting with different Aero and Space customers on various communication, Radar, and EWS system development through virtualization and digital engineering. Before joining ANSYS, he worked as a Research Consultant at GE Global Research in Bangalore in the area of Wireless Power Transfer and microwave material sensing for detecting different materials in Crude Oils. Nijas did his Master’s degree in Electronics Science and Ph.D. under Prof. P. Mohanan from Cochin University of Science and Technology, Cochin, Kerala. He holds 20+ International publications and 20+ national and international conference publications.
         </p>
         <p style="text-align: justify;">
-        <b>Workshop Objectives:</b><br>
-            ● To familiarize participants with the fundamental principles of millimeter-wave MMIC
-            design and fabrication.<br>
-            ● To explore the unique challenges and considerations in utilizing MMICs for front-end
-            applications in aerospace and defense.<br>
-            ● To discuss recent advancements and emerging trends in millimeter-wave MMIC
-            technology.<br>
-            ● To facilitate knowledge sharing and networking among participants from academia,
-            industry, and government sectors.
-        </p>
-        <p style="text-align: justify;">
-        <b>Workshop Format:</b><br>
-            The workshop will be conducted as a 2-hour comprehensive presentation along with panel
-            discussion. A detailed review of various industry and academia work in various aspects of
-            millimeter-wave MMICs and their applications will be presented.
-        </p>
-        <p style="text-align: justify;">
-        <b>Proposed Agenda:</b><br>
-            Introduction to Millimeter-Wave MMIC Technology<br>
-            ● Overview of millimeter-wave frequency bands and applications.<br>
-            ● Basics of MMIC design, fabrication, and packaging.<br>
-            Design Considerations for Aerospace and Defense Applications<br>
-            ● Requirements for high-frequency, high-power, and low-noise operation.<br>
-            ● Thermal management and reliability considerations.<br>
-            ● Integration challenges in front-end systems.<br>
-            Applications of Millimeter-Wave MMICs in Aerospace and Defense<br>
-            ● Radar systems: AESA, phased-array, and synthetic aperture radar.<br>
-            ● Communication systems: SATCOM, point-to-point links, and airborne networks.<br>
-            ● Electronic warfare (EW) systems: jamming, countermeasures, and signal
-            intelligence.<br>
-            Emerging III-V Technologies<br>
-            ● GaN, GaAs, InP MMIC technologies.<br>
-            Case Studies and Industry Perspectives<br>
-            ● Real-world examples of millimeter-wave MMIC deployments in aerospace and
-            defense platforms.<br>
-            ● Industry insights on current challenges and future opportunities.
-        </p>
-        <p style="text-align: justify;">
-        <b>Target Audience:</b><br>
-            The workshop is intended for engineers, researchers, and professionals working in the fields of
-            RF/microwave engineering, aerospace technology, defense systems, and related disciplines.
-            Participants with varying levels of expertise, from novice to experienced practitioners, are
-            welcome to attend.
-        </p>
-        <p style="text-align: justify;">
-        <b>Expected Outcomes:</b><br>
-            Enhanced understanding of millimeter-wave MMIC technology and its relevance to
-            aerospace and defense applications.<br>
-            Insight into the latest developments and future directions in the field of millimeter-wave
-            front-end systems.<br>
-            Opportunities for collaboration and knowledge exchange among participants and industry
-            experts.<br>
-            Inspiration for innovative solutions and research initiatives in millimeter-wave MMIC
-            design and integration.
-        </p>
-        <p style="text-align: justify;">
-        <b>Conclusion:</b><br>
-            This workshop aims to provide a platform for knowledge dissemination, collaboration, and
-            professional development in the rapidly evolving field of millimeter-wave MMICs for aerospace
-            and defense.
+            <b>Ravindra Challa:</b> Ravindra Challa is a Senior Application Engineer at Ansys, where he leverages more than 11 years of experience bringing AGI’s digital mission engineering and aerospace systems simulation solutions to the global aerospace and defense community. With a career built on applying technical solutions as a Scientist in Indian Space Research Organisation, Ravindra continues to apply a growing skill set to bring industry-leading simulation solutions to engineering organizations focused on digital transformation.
         </p>
       </div>
     </div>
@@ -134,7 +85,7 @@
 
     <?php include 'sponsors.php'; ?>
 
-    <!-- end sponsprs section -->
+    <!-- end sponsors section -->
 
     <div class="bottom_bg">
 
@@ -160,8 +111,7 @@
                                     <a href="https://twitter.com/ieeemapcon?lang=en" target="_blank">
                                         <i class="fa fa-twitter" aria-hidden="true"></i>
                                     </a>
-                                    <a href="https://www.linkedin.com/in/ieee-space-1859532bb/"
-                                        target="_blank">
+                                    <a href="https://www.linkedin.com/in/ieee-space-1859532bb/" target="_blank">
                                         <i class="fa fa-linkedin" aria-hidden="true"></i>
                                     </a>
                                 </div>
@@ -170,46 +120,31 @@
                         <div class="col-md-3">
                             <h3 class="f-header">Connect With IEEE</h3>
                             <ul>
-                                <li class="f-links"><a
-                                        href="https://ieee.taleo.net/careersection/2/jobsearch.ftl?utm_source=mf&utm_campaign=taleo-jobs&utm_medium=footer&utm_term=taleo-jobs%20at%20ieee">Careers
-                                        at IEEE</a></li>
+                                <li class="f-links"><a href="https://ieee.taleo.net/careersection/2/jobsearch.ftl?utm_source=mf&utm_campaign=taleo-jobs&utm_medium=footer&utm_term=taleo-jobs%20at%20ieee">Careers at IEEE</a></li>
                                 <li class="f-links"><a href="https://ieee-aess.org/">IEEE AESS</a></li>
-              <li class="f-links"><a href="https://ieeeaps.org/">IEEE APS</a></li>
+                                <li class="f-links"><a href="https://ieeeaps.org/">IEEE APS</a></li>
                                 <li class="f-links"><a href="https://mtt.org/">IEEE MTT-S</a></li>
-                                <li class="f-links"><a href="https://aps-mtts.ieeebangalore.org/">AP/MTT Joint Bangalore
-                                        Chapter</a></li>
-                                <li class="f-links"><a href="https://iln.ieee.org/public/TrainingCatalog.aspx">IEEE
-                                        Learning Network</a></li>
+                                <li class="f-links"><a href="https://aps-mtts.ieeebangalore.org/">AP/MTT Joint Bangalore Chapter</a></li>
+                                <li class="f-links"><a href="https://iln.ieee.org/public/TrainingCatalog.aspx">IEEE Learning Network</a></li>
                             </ul>
                         </div>
                         <div class="col-md-3">
                             <h3 class="f-header">Membership</h3>
                             <ul>
-                                <li class="f-links"><a
-                                        href="https://www.ieee.org/membership/join/index.php?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=join">Join</a>
-                                </li>
-                                <li class="f-links"><a
-                                        href="https://www.ieee.org/membership/renew.php?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=renew">Renew</a>
-                                </li>
-                                <li class="f-links"><a
-                                        href="https://www.ieee.org/membership/benefits/index.php">Benefits</a></li>
-                                <li class="f-links"><a
-                                        href="https://ieee-collabratec.ieee.org/?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=collabratec-membership-list">IEEE
-                                        Collabratec</a></li>
-                                <li class="f-links"><a href="https://ieeebangalore.org/guide-to-join-ieee/">Guide to
-                                        Join IEEE</a></li>
+                                <li class="f-links"><a href="https://www.ieee.org/membership/join/index.php?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=join">Join</a></li>
+                                <li class="f-links"><a href="https://www.ieee.org/membership/renew.php?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=renew">Renew</a></li>
+                                <li class="f-links"><a href="https://www.ieee.org/membership/benefits/index.php">Benefits</a></li>
+                                <li class="f-links"><a href="https://ieee-collabratec.ieee.org/?utm_source=dhtml_footer&utm_medium=hp&utm_campaign=collabratec-membership-list">IEEE Collabratec</a></li>
+                                <li class="f-links"><a href="https://ieeebangalore.org/guide-to-join-ieee/">Guide to Join IEEE</a></li>
                             </ul>
                         </div>
                         <div class="col-md-3">
                             <h3 class="f-header">Quick Links</h3>
                             <ul>
                                 <li class="f-links"><a href="https://ieeespace.org/">Home</a></li>
-                                <li class="f-links"><a href="https://ieeespace.org/organizing-committee/">Organizing
-                                        Committee</a></li>
-                                <li class="f-links"><a href="https://ieeespace.org/call-for-papers/">Call For
-                                        Papers</a></li>
-                                <li class="f-links"><a href="https://ieeespace.org/sponsorship.php">Sponsorship</a>
-                                </li>
+                                <li class="f-links"><a href="https://ieeespace.org/organizing-committee/">Organizing Committee</a></li>
+                                <li class="f-links"><a href="https://ieeespace.org/call-for-papers/">Call For Papers</a></li>
+                                <li class="f-links"><a href="https://ieeespace.org/sponsorship.php">Sponsorship</a></li>
                                 <li class="f-links"><a href="https://ieeespace.org/awards.php">Awards</a></li>
                             </ul>
                         </div>
@@ -249,8 +184,7 @@
         <!-- custom js -->
         <script src="js/custom.js"></script>
         <!-- Google Map -->
-        <script
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap"></script>
         <!-- End Google Map -->
 
 </body>
