f2fd101e3ed36b784dbd7dfd877c6239a7ce6406
Ketan K <ketan.keshav7@gmail.com>
2024-11-08 13:00:13 -0800

    Updating

--- important-dates/index.php
+++ important-dates/index.php
@@ -194,7 +194,7 @@
               </tr>
               <tr>
               <td><b>Last Date of Paper Submission</b></td>
-              <td>Feb 28, 2025</td>
+              <td><b>Feb 28, 2025</b></td>
              </tr>
               <tr>
                 <td>Notification of Acceptance</td>
