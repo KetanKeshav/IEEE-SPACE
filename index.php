65be894acb12f7b5256007a77e829fc5356d98d1
Ketan K <ketan.keshav7@gmail.com>
2024-11-28 03:14:42 -0500

    Updating

--- index.php
+++ index.php
@@ -53,11 +53,11 @@
       <div class="carousel-item active slide-1" style="background-color: #f0f0f0;">
         <div class="container">
           <div class="row">
-            <br><br><br><br><br>
-            <br><br><br><br><br>
-            <br><br><br><br><br>
             <div class="col-md-8 mx-auto">
               <div class="detail_box">
+                <br><br><br><br><br>
+                <br><br><br><br><br>
+                <br><br><br><br><br>
                 <a href="https://edas.info/N33216" target="_blank" style="color: #333; text-decoration: none;">
                   Submit Paper
                 </a>
